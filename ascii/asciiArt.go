ae3cbb43d5d1908f2ac0b76166e2b8c9898c4286
fonditi <konditi1.com>
2024-06-10 09:11:14 +0300

    first correct output

--- ascii/asciiArt.go
+++ ascii/asciiArt.go
@@ -7,14 +7,10 @@ import (
 )
 
 // Ascii prints ASCII art from a given array of characters.
-// The characters are extracted from a predefined file array.
-// The function takes in four arguments: fileArr (a slice of strings representing the file array),
-// wordsArr (a slice of strings representing the words to be printed),
-// lettersToColor (a string representing the letters to be colored),
-// and color (a string representing the color to be applied).
 func Ascii(fileArr []string, wordsArr []string, lettersToColor string, colorCode string, outputflag string) {
 	var count int
 	reset := "\033[0m"
+	var outputBuilder strings.Builder
 
 	for _, val := range wordsArr {
 		if val != "" {
@@ -22,62 +18,36 @@ func Ascii(fileArr []string, wordsArr []string, lettersToColor string, colorCode
 				for _, v := range val {
 					start := (v - 32) * 9
 					if len(lettersToColor) == 0 {
-						if IsFlagPassed("output") {
-							err := os.WriteFile(outputflag, []byte(fileArr[int(start)+i]), 0644)
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-						} else {
-							fmt.Print(colorCode + fileArr[int(start)+i] + reset)
-						}
-
+						outputBuilder.WriteString(colorCode + fileArr[int(start)+i] + reset)
 					} else if strings.Contains(lettersToColor, string(v)) {
-						if IsFlagPassed("output") {
-							err := os.WriteFile(outputflag, []byte(fileArr[int(start)+i]), 0644)
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-						} else {
-							fmt.Print(colorCode + fileArr[int(start)+i] + reset)
-						}
+						outputBuilder.WriteString(colorCode + fileArr[int(start)+i] + reset)
 					} else {
-						if IsFlagPassed("output") {
-							err := os.WriteFile(outputflag, []byte(fileArr[int(start)+i]), 0644)
-							if err != nil {
-								fmt.Println(err)
-								return
-							}
-						} else {
-							fmt.Print(fileArr[int(start)+i])
-						}
+						outputBuilder.WriteString(fileArr[int(start)+i])
 					}
 				}
-
-				if IsFlagPassed("output") {
-					err := os.WriteFile(outputflag, []byte("\n"), 0644)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-				} else {
-					fmt.Println()
-				}
+				outputBuilder.WriteString("\n")
 			}
 		} else {
 			count++
 			if count < len(wordsArr) {
-				if IsFlagPassed("output") {
-					err := os.WriteFile(outputflag, []byte("\n"), 0644)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-				} else {
-					fmt.Println()
-				}
+				outputBuilder.WriteString("\n")
 			}
 		}
+	}
+
+	if IsFlagPassed("output") {
+		file, err := os.Create(outputflag)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		_, err = file.WriteString(outputBuilder.String())
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Print(outputBuilder.String())
 	}
 }
